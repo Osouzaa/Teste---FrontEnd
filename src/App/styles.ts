f822ddb4e616f42cce735e7471fbe303d74f03ea
Osouzaa <gabrielsouzaalves750@gmail.com>
2025-03-04 15:26:21 -0300

    styles: add styles AppContent

--- src/App/styles.ts
+++ src/App/styles.ts
@@ -2,20 +2,26 @@ import styled from "styled-components";
 
 export const AppContainer = styled.div`
   width: 100%;
-  background-color: #121212;
-  color: #FFF;
   height: 100vh;
+  background-color: #121212;
+  color: #FFFFFF;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 20px;
-`
+  gap: 1.25rem; 
+`;
 
 export const AppContent = styled.div`
+  max-width: 90rem;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 20px;
-`
+  gap: 1.25rem; 
+  background-color: #282828;
+  padding: 1.25rem; 
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+`;
