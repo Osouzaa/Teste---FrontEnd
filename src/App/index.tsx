6d6a168e1b81f941ca40da66f8e7d08405144de6
Osouzaa <gabrielsouzaalves750@gmail.com>
2025-03-04 15:23:42 -0300

    feat: add styled-components to pasta app

---
+++ src/App/index.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyles } from "../styles/global";
+import { AppContainer, AppContent } from "./styles";
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <AppContainer>
+        <AppContent>
+          <h1>Heeeloo</h1>
+        </AppContent>
+      </AppContainer>
+    </>
+  )
+}
---
+++ src/App/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  width: 100%;
+  background-color: #121212;
+  color: #FFF;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`
+
+export const AppContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`
